4b7eb26f80ada4e33c162152b30cb3962d08fcc2
Pritish-Sinha <bpb1789@gmail.com>
2023-11-30 13:42:07 +0530

    frontend #2

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Namaste Stays</h1>
+      <p className='mb-4 text-slate-700'>Namaste Stays facilitates hotel, motel, restaurant, and resort bookings, offering a comprehensive platform for India tourism.</p>
+      <p className='mb-4 text-slate-700'>
+      Focused on showcasing Indian cultural richness, Namaste Stays encourages exploration and bookings, contributing to local tourism growth.
+      </p>
+      <p className='mb-4 text-slate-700'>With an intuitive interface, Namaste Stays ensures a seamless experience and secure transactions, enhancing user satisfaction in travel planning.</p>
+    </div>
+  )
+}
