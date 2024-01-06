ac1e41d4a97a1d5f2913d0c311d508f0eeada399
Pritish-Sinha <bpb1789@gmail.com>
2024-01-06 17:31:34 +0530

    restructure phase 1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { persistor, store } from './redux/store.js';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
+     clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
+     authorizationParams={{
+      redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.app"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
       <App />
-    </PersistGate>
-  </Provider>
+    </Auth0Provider>
+  </React.StrictMode>
 );
