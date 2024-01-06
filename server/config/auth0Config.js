8c89b05fbfd64406237677ec1495ca16f7c6a865
Pritish-Sinha <bpb1789@gmail.com>
2024-01-06 20:53:27 +0530

    set auth base url

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,7 +2,7 @@ import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
     audience: "http://localhost:8000",
-    issuerBaseURL: "https://dev-03ifqltxbr6nn0hn.us.auth0.com",
+    issuerBaseURL: "https://dev-t2uh1ay4qp5l5kla.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
 
